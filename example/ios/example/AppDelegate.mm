1089e5aae662beb6e05b355b5464a557a8926fc7
Wiki <weiji.taoweiji@alibaba-inc.com>
2021-02-19 16:40:16 +0800

    我成功了

--- example/ios/example/AppDelegate.mm
+++ example/ios/example/AppDelegate.mm
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-//    UrlSignature("", NULL);
+    const char* result = UrlSignature("", NULL);
     return YES;
 }
 
---
+++ example/ios/example/cross.mm
@@ -0,0 +1,8 @@
+//
+//  cross.h
+//  example
+//
+//  Created by Wiki on 2021/2/19.
+//
+
+#i
