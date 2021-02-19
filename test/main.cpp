d68843c92b6c7f3ffbfd211708ab3b1d026f25c2
Wiki <weiji.taoweiji@alibaba-inc.com>
2021-02-19 21:08:12 +0800

    添加hash库

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "json/json.h"
+#include "md5.h"
 
 int main() {
     Json::Value json;
     json["name"] = "Wiki";
     json["age"] = 18;
     std::cout << json.toStyledString() << std::endl;
+    MD5 md5;
+    md5.add("hello",5);
+    std::cout << md5.getHash() << std::endl;
+    md5.reset();
+    std::cout << md5.getHash() << std::endl;
     return 0;
 }
